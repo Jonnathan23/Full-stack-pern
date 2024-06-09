b820aaf34c7d4228e9f853c56b48d019239796e4
Jonnathan23 <jsaquicela26@gmail.com>
2024-06-09 16:26:15 -0500

    configuracion del archivo tsconfig y otras dependencias

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sumar = void 0;
+function sumar() {
+    console.log('Sumar');
+}
+exports.sumar = sumar;
+//# sourceMappingURL=server.js.map
