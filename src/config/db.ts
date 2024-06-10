f54d153b9aca48ae16e4409eafb96ce5548e4325
Jonnathan23 <jsaquicela26@gmail.com>
2024-06-09 22:29:34 -0500

    Tabla products creada

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
+
 dotenv.config()
-console.log(process.env.DATABASE_URL)
-const db = new Sequelize(process.env.DATABASE_URL!)
+const db = new Sequelize(process.env.DATABASE_URL!,{
+    models: [__dirname + '/../models/**/*.ts']
+})
 
 export default db
 
